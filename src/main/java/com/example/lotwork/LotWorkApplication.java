4b7c15857055913b55fdc11c300b1b824d6e239f
Никита <Ployka322@yandex.ru>
2023-07-24 10:47:51 +0700

    start lot

---
+++ src/main/java/com/example/lotwork/LotWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.lotwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotWorkApplication.class, args);
+    }
+
+}
