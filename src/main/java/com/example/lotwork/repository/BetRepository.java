ad70fc6cd6eeef8ca4767366568c3057e38f8759
Никита <Ployka322@yandex.ru>
2023-08-03 15:23:59 +0700

    Start t

--- src/main/java/com/example/lotwork/repository/BetRepository.java
+++ src/main/java/com/example/lotwork/repository/BetRepository.java
@@ -13,7 +13,7 @@ public interface BetRepository extends CrudRepository<Bet, Integer> {
     @Query("SELECT new com.example.lotwork.controller.BetsController" +
             "(b.nameBidder,MAX(b.bidDate))FROM Bid b WHERE b.lot.id=:id" +
             " GROUP BY nameBidder ORDER BY COUNT(b.lot.id) DESC, LIMIT 1")
-    abstract Optional<BetsController> nameBedFrequentLot(@Param("id") int id);
+    Optional<BetsController> nameBedFrequentLot(@Param("id") int id);
 
 
 
